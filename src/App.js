cdf45f9a50d29c48f71b20fcc6c05c0258cf3d85
karanjhala <karanjhala@gmail.com>
2020-11-14 13:20:06 +0800

    create input, useState, initital div and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [HorOffset,setHorOffset] = useState(10)
+  const [VerOffset,setVerOffset] = useState(10)
+  const [BlurOffset,setBlurOffset] = useState(10)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="controls">
+        <label >Horizontal Length</label>
+        <input type="range" min="-200" max="200" value={HorOffset} onChange={(e) => setHorOffset(e.target.value)} />
+        <label >Vertical Length</label>
+        <input type="range" min="-200" max="200" value={VerOffset} onChange={(e) => setVerOffset(e.target.value)} />
+        <label >Blur</label>
+        <input type="range" min="0" max="200" value={BlurOffset} onChange={(e) => setBlurOffset(e.target.value)} />
+      </div>
+      <div className="output">
+        <div className="box"></div>
+      </div>
+
     </div>
   );
 }
