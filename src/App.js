3b7704881c7679f1063cd647d7b2a0d6b1971ede
karanjhala <karanjhala@gmail.com>
2020-11-14 13:39:05 +0800

    Add color input, state & output boxshadow element

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ function App() {
   const [HorOffset,setHorOffset] = useState(10)
   const [VerOffset,setVerOffset] = useState(10)
   const [BlurOffset,setBlurOffset] = useState(10)
+  const [ColorValue,setColorValue] = useState("#54565a")
   return (
     <div className="App">
       <div className="controls">
-        <label >Horizontal Length</label>
+        <label>Horizontal Length</label>
         <input type="range" min="-200" max="200" value={HorOffset} onChange={(e) => setHorOffset(e.target.value)} />
-        <label >Vertical Length</label>
+        <label>Vertical Length</label>
         <input type="range" min="-200" max="200" value={VerOffset} onChange={(e) => setVerOffset(e.target.value)} />
-        <label >Blur</label>
+        <label>Blur</label>
         <input type="range" min="0" max="200" value={BlurOffset} onChange={(e) => setBlurOffset(e.target.value)} />
+        <label>Color</label>
+        <input type="color" value={ColorValue} onChange={(e) => setColorValue(e.target.value)} />
       </div>
       <div className="output">
-        <div className="box"></div>
+        <div className="box" style={{boxShadow: `${HorOffset}px ${VerOffset}px ${BlurOffset}px ${ColorValue}`}}></div>
       </div>
 
     </div>
